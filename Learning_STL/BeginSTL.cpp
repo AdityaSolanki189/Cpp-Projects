094670f4c26a1dbbdb6ebca70ce77ae5c7b0a885
Aditya Solanki <aditya2ss283@gmail.com>
2020-05-05 16:35:18 +0530

    new commit

--- Learning_STL/BeginSTL.cpp
+++ Learning_STL/BeginSTL.cpp
@@ -3,6 +3,10 @@
 #include <algorithm>
 using namespace std;
 
+bool f(int x,int y){
+    return x>y;
+}
+
 int main(){
 
         //C++ Standard Template Library
@@ -11,7 +15,7 @@ int main(){
 
     cout<< A[1]<<endl;   //2
 
-    sort(A.begin() , A.end()) // O(NlogN)
+    sort(A.begin() , A.end()); // O(NlogN) - Ascending Order Sort
     //2,3,11,14
 
     bool present = binary_search(A.begin(), A.end(), 3); //true
@@ -26,15 +30,25 @@ int main(){
     A.push_back(100);
     A.push_back(100);
     A.push_back(123);
-    //2,3,11,14,100,100,100,100,123
+    //2,3,11,14,100,100,100,100,100,123
 
-    vector<int>::iterator it = lower_bound(A.begin(), A.end(), 100); // >=
-    vector<int>::iterator it2 = upper_bound(A.begin(),A.end(), 100); // >
+    auto it = lower_bound(A.begin(), A.end(), 100); // >=
+    auto it2 = upper_bound(A.begin(),A.end(), 100); // >
     
-    cout<< *it << " " << *it2 << endl; //
-    cout<< it2- it1<< endl; //4
+    cout<< *it << " " << *it2 << endl; //100 123
+    cout<< it2- it<< endl; //5
 
+    sort(A.begin(),A.end(), f);
 
+    for(int x: A){ //Without Reference
+        cout<< x <<" ";
+    }
+    cout<<endl;  
+    for(int &x: A){ //With Reference
+        x++;
+        cout<< x <<" ";
+    }
+    cout<<endl;
     
     return 0;
 }

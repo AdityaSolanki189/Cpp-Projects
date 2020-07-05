dd970628a5750f27228b3849eb5dd7d3c70bea77
Aditya Solanki <aditya2ss283@gmail.com>
2020-07-05 17:29:50 +0530

    new commit

--- CodeChef/ChefString.cpp
+++ CodeChef/ChefString.cpp
@@ -13,11 +13,12 @@ int main(){
         for(int i=0;i<N;i++){ 
             cin>>str[i];
         }
-        int skips = 0,f=0,s=0;
-        for(int i=0;(i+1) < N;i++){
+        int skips = 0;
+        for(int i=0;i < N && i+1 <N;i++){
+            int f=0,s=0;
             f = str[i];
             s = str[i+1];
-            skips = skips + abs(f-s) -1;
+            skips = skips + (abs(f-s) -1);
         }
         cout<<skips<<endl;
     }

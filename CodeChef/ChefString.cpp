6d4e7a9b5f8d4e0c6241e4a1d609d50e90dba5d6
Aditya Solanki <aditya2ss283@gmail.com>
2020-07-06 20:20:10 +0530

    correction

--- CodeChef/ChefString.cpp
+++ CodeChef/ChefString.cpp
@@ -14,13 +14,24 @@ int main(){
             cin>>str[i];
         }
         int skips = 0;
-        for(int i=0;i < N && i+1 <N;i++){
-            int f=0,s=0;
-            f = str[i];
-            s = str[i+1];
-            skips = skips + (abs(f-s) -1);
+        if(N%2 == 0){
+            for(int i=0;i<N-2;i++){
+                int f=0,s=0;
+                f = str[i];
+                s = str[i+1];
+                skips = skips + (abs(f-s) -1);  
+            }
+        }
+        else{
+            for(int i=0;i<N-1;i++){
+                int f1=0,s1=0;
+                f1 = str[i];
+                s1 = str[i+1];
+                skips = skips + (abs(f1-s1) -1);  
+            }
         }
         cout<<skips<<endl;
     }
     return 0;
 }
+

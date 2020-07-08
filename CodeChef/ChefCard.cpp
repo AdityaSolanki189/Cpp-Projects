183f8b5ff4dfd37caf24c533888e41755b7314c2
Aditya Solanki <aditya2ss283@gmail.com>
2020-07-08 21:52:49 +0530

    new commit

---
+++ CodeChef/ChefCard.cpp
@@ -0,0 +1,21 @@
+#import <iostream>
+
+using namespace std;
+
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int N; //number of rounds
+        cin>>N;
+        int winA=0,winB=0;
+        int A[N],B[N];
+        int sum1
+        for(int i=0;i<N;i++){
+            cin>>A[i]>>B[i];
+            
+        }
+
+    }
+    return 0;
+}
--- CodeChef/ChefString.cpp
+++ CodeChef/ChefString.cpp
@@ -10,27 +10,17 @@ int main(){
         int N;
         cin>>N;
         int str[N];
-        for(int i=0;i<N;i++){ 
+        for(int i=1;i<=N;i++){ 
             cin>>str[i];
         }
-        int skips = 0;
-        if(N%2 == 0){
-            for(int i=0;i<N-2;i++){
-                int f=0,s=0;
-                f = str[i];
-                s = str[i+1];
-                skips = skips + (abs(f-s) -1);  
-            }
+        int skip = 0,f,s;
+        for(int i=1;i<N;i++){
+            f=0;s=0;
+            f = str[i];
+            s = str[i+1];
+            skip += (abs(f-s)-1);
         }
-        else{
-            for(int i=0;i<N-1;i++){
-                int f1=0,s1=0;
-                f1 = str[i];
-                s1 = str[i+1];
-                skips = skips + (abs(f1-s1) -1);  
-            }
-        }
-        cout<<skips<<endl;
+        cout<<skip<<endl;
     }
     return 0;
 }

faf7a439c7708f69d5e0c5f9510a15a8e7776fdd
Aditya Solanki <aditya2ss283@gmail.com>
2020-07-09 20:37:02 +0530

    new commit

--- CodeChef/ChefCard.cpp
+++ CodeChef/ChefCard.cpp
@@ -1,7 +1,16 @@
-#import <iostream>
+#include <iostream>
 
 using namespace std;
 
+int power(int num){
+    int sum=0,rem=0;
+    while(num!=0){
+        rem = num%10;
+        sum += rem;
+        num = num/10;
+    }
+    return sum;
+}
 int main(){
     int T;
     cin>>T;
@@ -10,12 +19,31 @@ int main(){
         cin>>N;
         int winA=0,winB=0;
         int A[N],B[N];
-        int sum1
-        for(int i=0;i<N;i++){
+        int sum1;
+        for(int i=1;i<=N;i++){
             cin>>A[i]>>B[i];
-            
         }
-
+        for(int i=1;i<=N;i++){
+            if(power(A[i]) > power(B[i])){
+                winA++;
+            }
+            if(power(A[i]) < power(B[i])){
+                winB++;
+            }
+            if(power(A[i]) == power(B[i])){
+                winB++;
+                winA++;
+            }
+        }
+        if(winA>winB){
+            cout<<0<<" "<<winA<<endl;
+        }
+        else if(winA < winB){
+            cout<<1<<" "<<winB<<endl;
+        }
+        else{
+            cout<<2<<" "<<winA<<endl;
+        }
     }
     return 0;
 }

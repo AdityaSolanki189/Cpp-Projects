75451e2175b7505082bfdeff4e7d4f73788888da
Aditya Solanki <aditya2ss283@gmail.com>
2020-08-02 19:14:01 +0530

    remove .exe

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+    string* x, y; 
     cout << "Hello, world!" << endl;
     cout << "Greetings from Illinois!";
     return 0;

4080f9d7748eff98a623e4ef2f426d47fc341dd6
Aditya Solanki <aditya2ss283@gmail.com>
2020-08-11 21:24:00 +0530

    commit

--- Data_Structures/LinkLists.cpp
+++ Data_Structures/LinkLists.cpp
@@ -4,5 +4,32 @@ using namespace std;
 
 struct node{
     int data;
-    node *next;
+    node *link;
 };
+
+node* head;
+
+void append(int n){
+    head = NULL;
+    node* temp = new node();
+    temp->data = n;
+    temp->link = NULL;
+    head = temp;
+}
+
+void display(){
+    node *temp1 = head;
+    while (temp1 != NULL) { 
+        cout << temp1->data << " "; 
+        temp1 = temp1->link; 
+    } 
+}
+
+int main(){
+    int n;
+    cout<<"Enter any Integer : ";
+    cin>>n;
+    append(n);
+    display();
+    return 0;
+}

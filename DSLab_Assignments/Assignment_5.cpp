c0a5a9f043ee973d489d9a741f7e4934a1a794e3
Aditya Solanki <aditya2ss283@gmail.com>
2020-08-18 22:34:16 +0530

    commit

--- DSLab_Assignments/Assignment_5.cpp
+++ DSLab_Assignments/Assignment_5.cpp
@@ -4,59 +4,73 @@
 
 using namespace std;
 
-class string_op{
-    public:
-        void frequency(string);
-        void Delete(string);
-        void chardelete(string);
-        void palindrome(string);
-}; 
+class string_op {
+public:
+    void frequency(string);
+    void Delete(string);
+    void chardelete(string);
+    void palindrome(string);
+};
 
-void string_op :: frequency(string str){
-    cout<<"The String Is : "<<str<<endl;
-    cout<<"Enter A Character : ";
+void string_op::frequency(string str) {
+    cout << "The String Is : " << str << endl;
+    cout << "Enter A Character : ";
     char ch;
-    int count=0;
-    cin>>ch;
-    for(int i=0;i<str.length();i++){
-        if(tolower (ch) == tolower(str[i]))
+    int count = 0;
+    cin >> ch;
+    for (int i = 0; i < str.length(); i++) {
+        if (tolower(ch) == tolower(str[i]))
             count++;
     }
-    cout<<"Frequency = "<<count<<endl;
+    cout << "Frequency = " << count << endl;
 }
-void string_op :: Delete(string str){}
-void string_op :: chardelete(string str){}
-void string_op :: palindrome(string str){}
+void string_op::Delete(string str) {
+    int j, k;
+    cout << "The OG String : " << str << endl;
+    cout << "Enter Any 2 Positions you want to Delete : " << endl;
+    cin >> j >> k;
+    for (int i = j; i < str.length(); i++) {
+        str[i] = str[i + 1];
+    }
+    for (int i = k-2; i < str.length(); i++) {
+        str[i] = str[i + 1];
+    }
+    cout << "Our New String : " << str << endl;
+}
 
-int main(){
+
+void string_op::chardelete(string str) {}
+void string_op::palindrome(string str) {}
+
+int main() {
     string_op obj;
-    string str ;
-    cout<<"Enter any String : ";
-    getline(cin,str);
-    int ch,t=1;
-    
-    while(t!=0){
-        cout<<"Choose An Operation :-\n1.Frequency()\n2.Delete()\n3.CharDelete()\n4.Palindrome()\n5.Exit(0)\n";
-        cin>>ch;
-        switch (ch){
-            case 1:
-                obj.frequency(str);
-                break;
-            case 2:
-                obj.Delete(str);
-                break;
-            case 3:
-                obj.chardelete(str);
-                break;
-            case 4:
-                obj.palindrome(str);
-                break;
-            case 5:
-                t=0;
-                break;
-            default:
-                cout<<"Enter Value Again"<<endl;
-                break;
+    string str;
+    cout << "\nEnter any String : ";
+    getline(cin, str);
+    int ch, t = 1;
+
+    while (t != 0) {
+        cout << "Choose An Operation :-\n1.Frequency()\n2.Delete()\n3.CharDelete()\n4.Palindrome()\n5.Exit(0)\n";
+        cin >> ch;
+        switch (ch) {
+        case 1:
+            obj.frequency(str);
+            break;
+        case 2:
+            obj.Delete(str);
+            break;
+        case 3:
+            obj.chardelete(str);
+            break;
+        case 4:
+            obj.palindrome(str);
+            break;
+        case 5:
+            t = 0;
+            break;
+        default:
+            cout << "Enter Value Again" << endl;
+            break;
         }
     }
     return 0;

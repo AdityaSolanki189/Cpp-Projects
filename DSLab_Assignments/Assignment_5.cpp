ccde66212c3b4cffb0d46564fe78b4adcbb17d8b
Aditya Solanki <aditya2ss283@gmail.com>
2020-08-19 22:58:57 +0530

    brackets

--- DSLab_Assignments/Assignment_5.cpp
+++ DSLab_Assignments/Assignment_5.cpp
@@ -4,8 +4,7 @@
 
 using namespace std;
 
-class string_op
-{
+class string_op{
 public:
     void frequency(string);
     void Delete(string);
@@ -13,65 +12,53 @@ public:
     void palindrome(string);
 };
 
-void string_op::frequency(string str)
-{
+void string_op::frequency(string str){
     cout << "The String Is : " << str << endl;
     cout << "Enter A Character : ";
     char ch;
     int count = 0;
     cin >> ch;
-    for (int i = 0; i < str.length(); i++)
-    {
+    for (int i = 0; i < str.length(); i++){
         if (tolower(ch) == tolower(str[i]))
             count++;
     }
     cout << "Frequency = " << count << endl;
 }
 
-void string_op::Delete(string str)
-{
+void string_op::Delete(string str){
     int j, k;
     cout << "The OG String : " << str << endl;
     cout << "Enter Any 2 Positions you want to Delete : " << endl;
     cin >> j >> k;
-    if (j < k)
-    {
-        for (int i = j; i < str.length(); i++)
-        {
+    if (j < k)    {
+        for (int i = j; i < str.length(); i++){
             str[i] = str[i + 1];
         }
 
-        for (int i = k - 1; i < str.length(); i++)
-        {
+        for (int i = k - 1; i < str.length(); i++){
             str[i] = str[i + 1];
         }
         cout << "The New String : " << str << endl;
     }
-    else if (j == k)
-    {
-        for (int i = k; i < str.length(); i++)
-        {
+    else if (j == k)    {
+        for (int i = k; i < str.length(); i++){
             str[i] = str[i + 1];
         }
         cout << "The New String : " << str << endl;
     }
-    else
-    {
-        for (int i = j; i < str.length(); i++)
-        {
+    else{
+        for (int i = j; i < str.length(); i++){
             str[i] = str[i + 1];
         }
 
-        for (int i = k; i < str.length(); i++)
-        {
+        for (int i = k; i < str.length(); i++){
             str[i] = str[i + 1];
         }
         cout << "The New String : " << str << endl;
     }
 }
 
-void string_op::chardelete(string str)
-{
+void string_op::chardelete(string str){
     cout << "The OG String : " << str << endl;
     char ch, new_str[100];
     cout << "Enter Any Character From The String : ";
@@ -86,37 +73,34 @@ void string_op::chardelete(string str)
 
 void string_op::palindrome(string str) {}
 
-int main()
-{
+int main(){
     string_op obj;
     string str;
     cout << "\nEnter any String : ";
     getline(cin, str);
     int ch, t = 1;
 
-    while (t != 0)
-    {
+    while (t != 0){
         cout << "\nChoose An Operation :-\n1.Frequency()\n2.Delete()\n3.CharDelete()\n4.Palindrome()\n5.Exit(0)\n";
         cin >> ch;
-        switch (ch)
-        {
-        case 1:
-            obj.frequency(str);
+        switch (ch){
+            case 1:
+                obj.frequency(str);
             break;
-        case 2:
-            obj.Delete(str);
+            case 2:
+                obj.Delete(str);
             break;
-        case 3:
-            obj.chardelete(str);
+            case 3:
+                obj.chardelete(str);
             break;
-        case 4:
-            obj.palindrome(str);
+            case 4:
+                obj.palindrome(str);
             break;
-        case 5:
-            t = 0;
+            case 5:
+                t = 0;
             break;
-        default:
-            cout << "Enter Value Again" << endl;
+            default:
+                cout << "Enter Value Again" << endl;
             break;
         }
     }

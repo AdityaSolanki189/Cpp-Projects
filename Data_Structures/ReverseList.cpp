e0cb4fcfe6c2078510916e2bb6f0ef36ad6c1b4f
Aditya Solanki <aditya2ss283@gmail.com>
2020-08-28 23:54:22 +0530

    commit

--- Data_Structures/ReverseList.cpp
+++ Data_Structures/ReverseList.cpp
@@ -1,13 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 struct Node{
     int data;
     Node* next;
 };
-Node* head;
 
-void Insert(int data){
+Node* Reverse( Node* head){
+    Node *current, *prev, *next;
+    current = head;
+    prev = NULL;
+    while (current != NULL){
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    head = prev;
+    return head;
+}
+
+Node* Insert(Node* head, int data){
     Node* temp = new Node();
     temp->data = data; temp->next = NULL;
     if (head == NULL) head = temp;
@@ -16,45 +29,30 @@ void Insert(int data){
     while (temp1->next != NULL) temp1 = temp1->next;
     temp1->next = temp;
     }
+    return head;
 }
 
-void Reverse(){
-    Node *current, *prev, *next;
-    current=head;
-    prev=NULL;
-    while(current!=NULL){
-        next = current->next;
-        current->next = prev;
-        prev = current;
-        current = next;
+void Print(Node* head){
+    while (head != NULL){
+    printf("%d ", head->data);
+    head = head->next;
     }
-    head = prev;
-}
-
-void print(){
-    Node* temp = head;
-    cout<<"The List : "<<endl;
-    while(temp != NULL){
-        cout<<temp->data<<" ";
-        temp = temp->next;
-    }
-    cout<<endl;
+    printf("\n");
 }
 
 int main(){
-    Node* head = NULL;  //empty head node
-    int n,x;
-    cout<<"Enter No. Of Elements : ";
+    Node* head = NULL;  //local variable head pointing NULL
+    int n,elem;
+    cout<<"\nEnter The Number Of Elements : ";
     cin>>n;
+    cout<<"\nEnter Elements : ";
     for(int i=0;i<n;i++){
-        cout<<"\nEnter The Element : ";
-        cin>>x;
-        Insert(x);
+        cin>>elem;
+        head = Insert(head,elem);
     }
-    cout<<"The Orginal List : "<<print();
-    for(int i=0;i<n;i++){
-        Reverse();
-    }
-    cout<<"The List After Reversing : "<<print();
-    return 0;
+    cout<<"\nThe Qriginal List : ";
+    Print(head);
+    head = Reverse(head);
+    cout<<"\nThe Reversed List : ";
+    Print(head);
 }

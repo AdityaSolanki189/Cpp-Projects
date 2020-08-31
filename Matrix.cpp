95e45a008b42f1474f5bbde21f961aa457678563
Aditya Solanki <aditya2ss283@gmail.com>
2020-08-31 10:49:29 +0530

    commit

--- Matrix.cpp
+++ Matrix.cpp
@@ -3,27 +3,38 @@ using namespace std;
 
 class matrix{
     protected:
-        int a[10][10],rows,columns;
+        int a[10][10],r[10][10],rows,columns;
     public:
-        void get();
-        void display();
-
+        void get(int,int);
+        void display(int,int);
+        void addition(int,int);
 };
 
+void matrix :: get(int r,int c){
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cin>>a[i][j];
+        }
+    }
+}
+void matrix :: display(int r,int c){
+        
+}
 int main(){
-    char op;
-	matrix a, b, c;
-	int t = 1;
+    char opt;
+	matrix obj;
+	int t = 1,rows,columns;
 	while (t) {
 		cout << "Select Option :\n1.Matrix Addition\n2.Matrix Subtration\n3.Matrix Multiplication\n4.Matrix Transponse\n5.Exit\n";
-		cin >> op;
-		switch (op) {
+		cin >> opt;
+		switch (opt) {
 		case '1':
 			cout << "\nMatrix Addiation\n";
-			a.get();
-			b.get();
-			c = a + b;
-			c.put();
+            cout<<"Enter Matrix Dimensions : ";
+            cin>>rows>>columns;
+			obj.get(rows,columns);
+            obj.add(rows,columns);
+            obj.display(rows,columns);
 			break;
 
 		case '2':

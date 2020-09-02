44eadddbe93ffabed9c10d53316f172c67c5398f
Aditya Solanki <aditya2ss283@gmail.com>
2020-09-02 22:57:02 +0530

    commit

--- DSLab_Assignments/Assignment_2.cpp
+++ DSLab_Assignments/Assignment_2.cpp
@@ -3,30 +3,34 @@ using namespace std;
 # define s 20
 
 /* A saddle point is an element of the matrix such that it 
-is the minimum element in its row and maximum in its column. */
+is the minimum element in its row[m] and maximum in its column[n]. */
 
 void saddle_point(int arr[s][s],int m,int n){
+    int count=0;
     for(int i=0;i<m;i++){
-        int min_row = arr[i][0], column_id = 0,j,k;
-        //finding minimum of the row
+        int min_row = arr[i][0], column_index = 0,j,k;
+        //finding minimum of the row[m]
         for (j = 1; j < n; j++) { 
             if (min_row > arr[i][j]) { 
                 min_row = arr[i][j]; 
-                column_id = j; 
+                column_index = j; 
             } 
         }
-        //finding maximum of the column
-        for(k=0;i<m;k++){
-            if (min_row < arr[k][column_id]) 
+        //finding maximum of the column[n]        
+        for(k=0;i<n;k++){
+            if (min_row < arr[k][column_index]) {
                 break;
+            }
+            if (k == n-1) { 
+                cout << "\nSaddle Point Of The Given Matrix Is : " << min_row<<endl;
+                cout << "At Location : "<<"["<<i+1<<","<<column_index+1<<"]"<<endl; 
+                count++;
+            } 
         }
-        if (k == m) { 
-           cout << "\nSaddle Point Of The Given Matrix Is : " << min_row<<endl;
-           cout << "At Location : "<<"["<<i+1<<","<<column_id+1<<"]"<<endl;
-           return; 
-        } 
     }
-    cout<<"\nSaddle Point Not Found!\n";
+    if(count == 0){
+        cout<<"\nSaddle Point Not Found In The Given Matrix!\n";
+    }
 }
 
 int main(){

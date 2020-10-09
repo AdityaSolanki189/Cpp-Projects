8ed8a6f366f364caa5e8866ec98a033824a635a6
Aditya Solanki <aditya2ss283@gmail.com>
2020-10-09 21:27:01 +0530

    commit

---
+++ CodeChef/replaceforx.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int n,x,p,k;
+		cin>>n>>x>>p>>k;
+		int arr[n];
+		for(int i=0;i<n;i++) cin>>arr[i];
+		
+	}
+}

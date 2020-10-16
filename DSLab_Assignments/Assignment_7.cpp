815e71c09d76f2740f59599531887dca38cf4621
Aditya Solanki <aditya2ss283@gmail.com>
2020-10-16 15:44:37 +0530

    commit

---
+++ DSLab_Assignments/Assignment_7.cpp
@@ -0,0 +1,152 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+struct Node{
+    string data;
+    Node *link;
+};
+
+class List{
+    public:
+        int pos,count1=0,cont=0;
+        string ch1 = "club";
+        string ch2,ch;
+        Node *head,*tail,*prev,*cur;
+        Node *create_node();
+        void create_list();
+        void operation();
+        void display();
+        void data();
+        List(){
+            head = NULL;
+            tail = NULL;
+        }
+};
+
+Node *List :: create_node(){
+    Node *new_node = new Node();
+    prev = NULL;
+    cur= head;
+    cout<<"Enter Member's Name : "<<endl;
+    cin.ignore();
+    getline(cin,new_node->data);
+    new_node->link = NULL;
+    count1= count1+1;
+    return (new_node);
+}
+
+void List :: display(){
+    Node *temp;
+    temp = head;
+    if(temp==NULL){
+        cout<<"list Is Empty"<<endl;
+        return;
+    }
+    else{
+        while(temp!=NULL){
+            cout<<temp->data<<" "<<endl;
+            temp = temp->link;
+        }
+    }
+}
+
+void List :: create_list(){ 
+    Node *new_Node;
+    char choice_am;
+    char temp = 'y';
+    string pswd_user;
+    string pswd = "club";
+    cout<<"Are You Admin or Memeber? (A/M)"<<endl;
+    cin>>choice_am;
+    if(choice_am == 'A'|| choice_am == 'a'){
+        cout<<"Enter Club's Password : ";
+        cin>>pswd_user;
+        while(temp == 'y'){
+            if(pswd == pswd_user){
+                cout<<"Access Granted!"<<endl;
+                while(temp == 'y'){
+                    new_Node = create_node();
+                    if(head == NULL){
+                        head = new_Node;
+                        tail = new_Node;
+                    }
+                    else{
+                        tail->link = new_Node;
+                        tail = new_Node;    
+                    }
+                    cout<<"Do you want to Enter More Members ? (y/n)"<<endl;
+                    cin>>temp;
+                }
+            }
+            else{
+                temp = 'y';
+                cout<<"Incorrect Password!"<<endl;
+                cout<<"Do you want to TRY AGAIN ? (y/n)";
+                cin>>temp;
+                if(temp == 'n') return;
+            }
+        }
+    }
+    else{
+        cout<<"You Cannot Edit The Members List!"<<endl;
+    }
+}
+
+int main(){
+    List obj;
+    cout<<"\nWelcome To Pinnacle Club\n"<<endl;
+    obj.create_list();
+    //obj.operation();
+    return 0;
+    /* while(t!=0){
+        cout<<"\nClub Member Operations :\na) Insert at Beggining;\nb) Insert at End;\nc) Insert at N;\nd) Delete at Beggining;\ne) Exit(0)"<<endl;
+        int ch;
+        cin>>ch;
+        switch (ch){
+            case 1:
+                obj.insert_begg();
+                obj.display();
+            break;
+
+            case 2:
+                obj.insert_end();
+                obj.display();
+            break;
+
+            case 3:
+                obj.insert_n();
+                obj.display();
+            break;
+
+            case 4:
+                obj.delete_begg();
+                obj.display();
+            break;
+
+            case 5:
+                obj.delete_end();
+                obj.display();
+            break;
+
+            case 6:
+                obj.delete_n();
+                obj.display();
+            break;
+
+            case 7:
+                obj.reverse();
+            break;
+
+            case 8:
+                t = 0;
+            break;
+
+            default:
+                cout<<"Invalid Input";
+            break;
+
+        }
+    }
+    return 0; */
+}
--- PracticeCodes/Exercise.cpp
+++ PracticeCodes/Exercise.cpp
@@ -1,12 +1,16 @@
 #include<iostream>
-
+#include<string>
 using namespace std;
 
 int main(){
     int a,b,c;
+    string str;
     cout<<"Heellooo Woorlldd!\n";
     cin>>a>>b;
+    cout<<"Enter Your String : "<<endl;
+    cin.ignore();
+    getline(cin,str);
     c = a+b;
-    cout<<c;
+    cout<<c<<str;
     return 0;
 }

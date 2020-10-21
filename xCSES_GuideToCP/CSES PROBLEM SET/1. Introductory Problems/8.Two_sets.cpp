4a879f7f8e5f37ad1fe68c841b29e6d8d4dcfb45
Aditya Solanki <aditya2ss283@gmail.com>
2020-10-21 22:33:12 +0530

    commit

--- xCSES_GuideToCP/CSES PROBLEM SET/1. Introductory Problems/8.Two_sets.cpp
+++ xCSES_GuideToCP/CSES PROBLEM SET/1. Introductory Problems/8.Two_sets.cpp
@@ -17,10 +17,15 @@ int main(){
 	cout.tie(0);
 	ios_base::sync_with_stdio(0);
 
-	ll num;
+	ll num,sum,wsum;
 	cin>>num;
     vector<int> x;
-    
+
+    if(n*(n+1) %4 != 0){
+    	cout<<"NO";
+    	return 0;
+    }
+
 
 
 	return 0;

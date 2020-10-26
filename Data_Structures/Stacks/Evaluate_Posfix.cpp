93505c976c4f799f5c0fd40550a99aaa250c1577
Aditya Solanki <aditya2ss283@gmail.com>
2020-10-26 23:33:25 +0530

    COMMIT

--- Data_Structures/Stacks/Evaluate_Posfix.cpp
+++ Data_Structures/Stacks/Evaluate_Posfix.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <String>
+#include <cmath>
 #include <stack>
 using namespace std;
 
@@ -16,19 +17,19 @@ int Calculate(char optr, int op1, int op2) {
 
 int Evaluate_PostFix(string exp) {
     stack <int> S;
-    char op1, op2;
+    int op1, op2;
     int res = 0;
-    for (int i = 0; i < exp.length() - 1; i++) {
+    for (int i = 0; i <= exp.length()-1; i++) {
         if (exp[i] == '+'|| exp[i] == '-' || exp[i] == '*' || exp[i] == '/') {
             op1 = S.top();
             S.pop();
             op2 = S.top();
             S.pop();
-            res = Calculate(exp[i],(int)op1,(int)op2);
+            res = Calculate(exp[i],op1,op2);
             S.push(res);
         }
         else {
-            S.push((int)exp[i]);
+            S.push(exp[i] - '0'); //CONVERT CHAR NUMBER TO INT NUMBER 
         }
     }
     return S.top();
@@ -37,8 +38,8 @@ int Evaluate_PostFix(string exp) {
 int main() {
     string exp;
     cout << "Enter A PostFix String : ";
-    getline(cin, exp);
-    cout << Evaluate_PostFix(exp);
+    cin>>exp;
+    cout << "Answer Of PostFix : "<< Evaluate_PostFix(exp);
     return 0;
 }
 

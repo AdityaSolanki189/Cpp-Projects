8615c7f31518ab6f5c3c311bcd225c6f79f9341e
Aditya Solanki <aditya2ss283@gmail.com>
2020-10-27 21:21:07 +0530

    commit

--- Data_Structures/Stacks/Evaluate_Prefix.cpp
+++ Data_Structures/Stacks/Evaluate_Prefix.cpp
@@ -19,7 +19,7 @@ int Evaluate_PostFix(string exp) {
     stack <int> S;
     int op1, op2;
     int res = 0;
-    for (int i = exp.length() - 1; i >0 ; i--) {
+    for (int i = exp.length() - 1; i >=0 ; i--) {
         if (exp[i] == '+' || exp[i] == '-' || exp[i] == '*' || exp[i] == '/') {
             op1 = S.top();
             S.pop();
@@ -44,6 +44,6 @@ int main() {
 }
 
 /*Sample Input :
-    Enter A PostFix String : 23*54*+9-
+    Enter A Prefix String : -+*23*549
     Answer Of Postfix : 17
 */

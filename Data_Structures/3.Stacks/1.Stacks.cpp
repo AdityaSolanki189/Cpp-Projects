accadb95a700cdfe1cb070912848e34f8e99420b
Aditya Solanki <aditya2ss283@gmail.com>
2020-10-31 20:44:42 +0530

    commit

--- Data_Structures/3.Stacks/1.Stacks.cpp
+++ Data_Structures/3.Stacks/1.Stacks.cpp
@@ -1,9 +1,50 @@
+//Without Using the STL library!
 #include <iostream>
 using namespace std;
 
+class stack{
 
+    public:
+    void create();
+    void push();
+    void pop();
+    void top();
+    void display();
+};
 
 int main(){
-
+    stack obj;
+    cout<<"\n~ STACK OPERATIONS ~\n\n1. Create a Stack\n2. Push a Value\n3. Pop a Value\n4. Display Stack Contents\n5. Display top\n"<<endl;
+    int t=1,ch;
+    while(t){
+        cin>>ch;
+        switch(ch){
+            case 1: 
+                obj.create();
+                obj.display();
+            break;
+            case 2: 
+                obj.push();
+                obj.display();
+            break;
+            case 3: 
+                obj.pop();
+                obj.display();
+            break;
+            case 4: 
+                obj.display();
+                obj.display();
+            break;
+            case 5: 
+                obj.top();
+            break;
+            case 6: 
+                t=0;
+            break;
+            default: 
+                cout<<"Invalid Input !"<<endl;
+            break;
+        }
+    }
     return 0;
 }

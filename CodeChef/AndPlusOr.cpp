a9f540e1afbe6b3c0c16e4f40b7f2ed49c11fce5
Aditya Solanki <aditya2ss283@gmail.com>
2020-11-01 22:16:13 +0530

    new commit

---
+++ CodeChef/AndPlusOr.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+typedef long long int ll;
+int main(){
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+    ll t;
+    cin>>t;
+    while(t--){
+        ll x;
+        cin>>x;
+        cout<<"0 "<<x<<"\n";
+    }
+    return 0;
+}

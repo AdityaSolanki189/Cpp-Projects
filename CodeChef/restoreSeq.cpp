cd6cb3082866b343e1b279079ab54131110fa2fa
Aditya Solanki <aditya2ss283@gmail.com>
2020-11-10 21:13:13 +0530

    commit

---
+++ CodeChef/restoreSeq.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int B[N];
+        for(int i=1;i<=N;i++) cin>>B[i];
+        
+    }
+
+    return 0;
+}

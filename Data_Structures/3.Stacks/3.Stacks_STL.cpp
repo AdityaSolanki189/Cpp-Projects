23f9c74b134a2bc4a649bbd535ad0eeb3dfba61b
Aditya Solanki <aditya2ss283@gmail.com>
2020-11-25 21:02:56 +0530

    commit

---
+++ Data_Structures/3.Stacks/3.Stacks_STL.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    stack<int> s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    cout<<s.size()<<endl;
+    cout<<s.top()<<endl;
+    s.pop();
+    cout<<s.top()<<endl;
+    s.push(5);
+    cout<<s.top()<<endl;
+    
+    while(s.empty()==false){
+        cout<<s.top()<<endl;
+        s.pop();
+    }
+    return 0; 
+}
---
+++ Data_Structures/4.Queues/3.Queue_Reverse.cpp
@@ -0,0 +1,38 @@
+#include <iostream> 
+#include <queue>
+#include <stack>
+using namespace std; 
+
+
+void Print(queue<int>& Queue) { 
+	while (!Queue.empty()) { 
+		cout << Queue.front() << " "; 
+		Queue.pop(); 
+	} 
+} 
+
+
+void reverseQueue(queue<int>& Queue) { 
+	stack<int> Stack; 
+	while (!Queue.empty()) { 
+		Stack.push(Queue.front()); 
+		Queue.pop(); 
+	} 
+	while (!Stack.empty()) { 
+		Queue.push(Stack.top()); 
+		Stack.pop(); 
+	} 
+} 
+
+ 
+int main() { 
+	queue<int> q; 
+	q.push(12); 
+	q.push(5); 
+	q.push(15);
+	q.push(20); 
+
+	reverseQueue(q); 
+	Print(q); 
+    return 0;
+} 

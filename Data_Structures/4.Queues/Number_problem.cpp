c228df14224c7611c175b187459fbbb18c12d34f
Aditya Solanki <aditya2ss283@gmail.com>
2020-11-26 15:27:27 +0530

    commit

--- Data_Structures/4.Queues/Number_problem.cpp
+++ Data_Structures/4.Queues/Number_problem.cpp
@@ -1,1 +1,33 @@
 /* Given a number n, print first n number(in increasing order) such that all these numbers have digits in set {5, 6} */
+#include <iostream> 
+#include <queue>
+using namespace std; 
+
+void printFirstN(int n) { 
+    queue<string> q;
+    
+    q.push("5");
+    q.push("6");
+    
+    for(int i = 0; i < n; i++) {
+        string curr = q.front();
+       
+        cout << curr << " ";
+        
+        q.pop();
+        
+        q.push(curr + "5");
+        q.push(curr + "6");
+    }
+    cout<<"\n";
+} 
+
+int main(){
+    
+    int n;
+    cout<<"\nEnter Any Number : ";
+    cin>>n;
+    printFirstN(n);
+
+    return 0;
+}

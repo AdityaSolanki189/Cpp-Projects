c8b69ca449febfb6031ec977577431a35050eb61
Aditya Solanki <aditya2ss283@gmail.com>
2020-11-28 20:54:34 +0530

    commit

---
+++ CodeChef/gasoline_adv.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int test;
+    cin>>test;
+    while(test--){
+        int N;
+        cin>>N;
+        int fuel[N];
+        for(int i=0;i<N;i++)cin>>fuel[i];
+        int cost[N];
+        for(int i=0;i<N;i++)cin>>cost[i];
+
+        
+           
+    }
+    return 0;
+}

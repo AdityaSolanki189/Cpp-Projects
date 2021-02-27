9fb7da8258eb8e7a08bbb41df75ba706bfecef82
Aditya Solanki <aditya2ss283@gmail.com>
2021-02-27 15:50:33 +0530

    commit

---
+++ Puzzels_/nQueensProblem.cpp
@@ -0,0 +1,11 @@
+/* The n-Queens Problem Place n queens on an n × n chessboard so that no two queens
+attack each other by being in the same column, row, or diagonal. */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+
+    return 0;
+}

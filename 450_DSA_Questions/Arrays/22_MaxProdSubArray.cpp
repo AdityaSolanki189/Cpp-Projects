3d12bc548280f026dd36a3acf9bb69b746f2484d
Aditya Solanki <aditya2ss283@gmail.com>
2021-03-08 19:26:28 +0530

    commit

---
+++ 450_DSA_Questions/Arrays/22_MaxProdSubArray.cpp
@@ -0,0 +1,17 @@
+/* Given an array Arr that contains N integers (may be positive, negative or zero). Find the product of the maximum product subarray.
+Example 1:
+    Input:
+    N = 5
+    Arr[] = {6, -3, -10, 0, 2}
+    Output: 180
+    Explanation: Subarray with maximum product
+    is  6, -3, -10 which gives product as 180. 
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    return 0;
+}

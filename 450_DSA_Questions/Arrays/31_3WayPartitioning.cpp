c17800c4b7d6b3ac873ac6891ebee3088072b610
Aditya Solanki <aditya2ss283@gmail.com>
2021-03-12 00:46:28 +0530

    Arrays Part 3

---
+++ 450_DSA_Questions/Arrays/31_3WayPartitioning.cpp
@@ -0,0 +1,25 @@
+/* Given an array of size N and a range [a, b]. The task is to partition the array around the range such that array is divided into three parts.
+1) All elements smaller than a come first.
+2) All elements in range a to b come next.
+3) All elements greater than b appear in the end.
+The individual elements of three sets can appear in any order. You are required to return the modified array.
+
+Example 1:
+Input: 
+N = 5
+A[] = {1, 2, 3, 3, 4}
+[a, b] = [1, 2]
+Output: 1
+Explanation: One possible arrangement is:
+{1, 2, 3, 3, 4}. If you return a valid
+arrangement, output will be 1. */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+     
+    return 0;
+}

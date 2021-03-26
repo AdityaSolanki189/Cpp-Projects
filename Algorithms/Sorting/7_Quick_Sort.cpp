f1f68c42d1a5e0c7354307b3360d5ea3e8939435
Aditya Solanki <aditya2ss283@gmail.com>
2021-03-27 01:56:11 +0530

    Sorting Algorithms _ comparing

--- Algorithms/Sorting/7_Quick_Sort.cpp
+++ Algorithms/Sorting/7_Quick_Sort.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h> 
 using namespace std;  
   
-// A utility function to swap two elements  
 void swap(int* a, int* b){  
     int t = *a;  
     *a = *b;  
@@ -13,20 +12,45 @@ the pivot element at its correct position in sorted
 array, and places all smaller (smaller than pivot)  
 to left of pivot and all greater elements to right  
 of pivot */
+int partition (int arr[], int low, int high) 
+{ 
+    int pivot = arr[high]; // pivot 
+    int i = (low - 1); // Index of smaller element and indicates the right position of pivot found so far
+ 
+    for (int j = low; j <= high - 1; j++) 
+    { 
+        // If current element is smaller than the pivot 
+        if (arr[j] < pivot) 
+        { 
+            i++; // increment index of smaller element 
+            swap(&arr[i], &arr[j]); 
+        } 
+    } 
+    swap(&arr[i + 1], &arr[high]); 
+    return (i + 1); 
+}
+
+/* 
 int partition (int arr[], int low, int high){  
-    int pivot = arr[high]; // pivot  
-    int i = (low - 1); // Index of smaller element  
-  
-    for (int j = low; j <= high - 1; j++){  
-        // If current element is smaller than the pivot  
-        if (arr[j] < pivot){  
-            i++; // increment index of smaller element  
-            swap(&arr[i], &arr[j]);  
-        }  
-    }  
-    swap(&arr[i + 1], &arr[high]);  
-    return (i + 1);  
-}  
+    int left, right, pivot_item = arr[low];
+    left = high;
+    while(left < right){
+        // Move left while item < pivot
+        whlie(arr[left] <= pivot_item)
+            left++;
+        
+        // Move right while item > pivot
+        while(arr[right] > pivot_item)
+            right--;
+        
+        if(left < right)
+            swap(arr, left, right);
+    }
+    // right is final positoion for the pivot
+    arr[low] = arr[right];
+    arr[right] = pivot_item;
+    return right;
+}   */
   
 /* The main function that implements QuickSort  
 arr[] --> Array to be sorted,  
@@ -36,12 +60,12 @@ void quickSort(int arr[], int low, int high){
     if (low < high){  
         /* pi is partitioning index, arr[p] is now  
         at right place */
-        int pi = partition(arr, low, high);  
+        int pivot = partition(arr, low, high);  
   
         // Separately sort elements before  
         // partition and after partition  
-        quickSort(arr, low, pi - 1);  
-        quickSort(arr, pi + 1, high);  
+        quickSort(arr, low, pivot - 1);  
+        quickSort(arr, pivot + 1, high);  
     }  
 }  
 
@@ -57,6 +81,7 @@ void printArray(int arr[], int size){
     Best - Ω(n(logn))
     Avg. - θ(n(logn))
     Worst - O(n^2) 
+    Worst case space complexity : O(1)
 */
 
 int main(){  
@@ -67,3 +92,21 @@ int main(){
     printArray(arr, n);  
     return 0;  
 }  
+
+/* Theory:
+Quick sort is an example of a divide-and-conquer algorithmic technique. It is also called
+partition exchange sort. It uses recursive calls for sorting the elements, and it is one of the
+famous algorithms among comparison-based sorting algorithms.
+    Divide: The array A[low ...high] is partitioned into two non-empty sub arrays A[low ...q] and A[q + 1... high], such that each element of A[low ... high] is less than or equal to each element of A[q + 1... high]. The index q is computed as part of this partitioning procedure.
+    
+    Conquer: The two sub arrays A[low ...q] and A[q + 1 ...high] are sorted by recursive calls to
+    Quick sort.
+Algorithm
+The recursive algorithm consists of four steps:
+    1) If there are one or no elements in the array to be sorted, return.
+    2) Pick an element in the array to serve as the “pivot” point. (Usually the left-most
+    element in the array is used.)
+    3) Split the array into two parts – one with elements larger than the pivot and the other
+    with elements smaller than the pivot.
+    4) Recursively repeat the algorithm for both halves of the original array.
+*/
